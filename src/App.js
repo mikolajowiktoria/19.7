e15cc6596ab26eb945296e7a835cf955209d93be
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-04-04 20:21:52 +0200

    finished task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	return (
+		<div className="App">
+			Tutaj znajdzie się lista komentarzy!
+		</div>
+	);
+};
+
+export default App;
